e4a034d8401a55d435f89fc08823b7a54a3a8be2
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-12-28 23:53:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -125,9 +125,6 @@ def adding_to_dennis
           :known_for => "Unix",
           :languages => ["C"]
         }
-        
-        puts programmer_hash[:grace_hopper],
-puts programmer_hash[:alan_kay][:known_for]
      }
 
 end
